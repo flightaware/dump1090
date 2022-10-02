3e8968d0b379b551682d6b892064c19e83903fc7
Connor Burnett <burnettcj1966@gwu.edu>
2022-10-02 12:17:32 -0400

    basic peer-to-peer network

---
+++ networking/client.py
@@ -0,0 +1,49 @@
+import socket
+import sys
+import threading
+
+rendezvous = ('172.20.192.178', 55555)
+
+
+print('connecting to rendezvous server')
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('0.0.0.0', 50001))
+sock.sendto(b'0', rendezvous)
+
+while True:
+    data = sock.recv(1024).decode()
+
+    if data.strip() == 'ready':
+        print('checked in with server, waiting')
+        break
+
+data = sock.recv(1024).decode()
+ip, sport, dport = data.split(' ')
+sport = int(sport)
+dport = int(dport)
+
+print('\ngot peer')
+print('  ip:            {}'.format(ip))
+print('  source port:   {}'.format(sport))
+print('  dest port:     {}\n'.format(dport))
+
+print('ready to exchange messages\n')
+
+def listen():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('0.0.0.0', sport))
+
+    while True:
+        data = sock.recv(1024)
+        print('\rpeer: {}\n>'.format(data.decode()), end='')
+
+listener = threading.Thread(target = listen, daemon=True)
+listener.start()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('0.0.0.0', dport))
+
+while True:
+    msg = input('> ')
+    sock.sendto(msg.encode(), (ip, sport))
