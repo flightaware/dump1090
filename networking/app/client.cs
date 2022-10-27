02e8cb4f86a25065ca751a4387120221bf8582b9
Connor <burnettcj1966@gwu.edu>
2022-10-27 10:42:37 -0400

    Modifyed C# code to run on windows

--- networking/app/client.cs
+++ networking/app/client.cs
@@ -6,28 +6,11 @@ using System.Threading;
 
 public class client {
 
-    static NetworkStream stream;
-
     static void Main(string[] args)
     {
         String server_ip = "10.0.0.166";
         Int32 server_port = 55555;
         runClient(server_ip, server_port);
-    }
-
-    static void listen()
-        {
-            Byte[] message = new Byte[1024];
-            while (true)
-            {
-                String recv_message = String.Empty;
-                Int32 bytes = stream.Read(message, 0, message.Length);
-                recv_message = System.Text.Encoding.ASCII.GetString(message, 0, bytes);
-            if(!recv_message.Equals("")) 
-            {
-                Console.WriteLine(recv_message);
-            }
-        }
     }
 
     static void runClient(String server, Int32 port)
@@ -36,13 +19,32 @@ public class client {
             {
                 TcpClient client = new TcpClient(server, port);
 
-                stream = client.GetStream();
+                NetworkStream stream = client.GetStream();
 
                 Console.WriteLine("Socket Connected to: ", server);
 
-                Thread listener = new Thread(listen);
-                listener.start();
+                // Runs reader thread
+                // Todo: Modify the input to send data to C# objects
+                void listen()
+                {
+                    Byte[] message = new Byte[1024];
+                    while (true)
+                    {
+                        String recv_message = String.Empty;
+                        Int32 bytes = stream.Read(message, 0, message.Length);
+                        recv_message = System.Text.Encoding.ASCII.GetString(message, 0, bytes);
+                        if(!recv_message.Equals("")) 
+                        {
+                            Console.WriteLine(recv_message);
+                        }
+                    }
+                }
 
+                Thread listener = new Thread(listen);
+                listener.Start();
+
+                //Output
+                //Todo: Modify to only send an output when signal is processed for FPS loss
                 while (true)
                 {
                     Console.WriteLine(">");
